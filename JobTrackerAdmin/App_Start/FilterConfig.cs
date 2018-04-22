5c176cbdcc0d37f7a52a084dff47d6f0e324bcf7
kaustav2012sen <kaustav.bandreal@gmail.com>
2018-04-22 19:38:33 +0530

    first commit

---
+++ JobTrackerAdmin/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JobTrackerAdmin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ JobTrackerAdmin/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobTrackerAdmin.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult ClientList()
+        {
+            return View();
+        }
+
+        public ActionResult AddEditClient()
+        {
+            return View();
+        }
+    }
+}
