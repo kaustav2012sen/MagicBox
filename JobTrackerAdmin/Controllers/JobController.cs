2fd7e9e46597c04a4350cedd69e8fdc3ba007840
kaustav2012sen <kaustav.bandreal@gmail.com>
2018-04-23 09:20:00 +0530

    third commit

---
+++ JobTrackerAdmin/Controllers/JobController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobTrackerAdmin.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public ActionResult JobDetails()
+        {
+            return View();
+        }
+
+        public ActionResult AddEditJob()
+        {
+            return View();
+        }
+    }
+}
---
+++ JobTrackerAdmin/Controllers/ReportsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobTrackerAdmin.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult DailyReport()
+        {
+            return View();
+        }
+
+        public ActionResult MonthlyReport()
+        {
+            return View();
+        }
+
+        public ActionResult BillingDetails()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentTracker()
+        {
+            return View();
+        }
+    }
+}
