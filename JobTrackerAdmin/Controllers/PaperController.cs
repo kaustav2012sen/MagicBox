238d74c4d44b7aae5702e1e00b3d407eed9781e3
kaustav2012sen <kaustav.bandreal@gmail.com>
2018-05-03 09:23:25 +0530

    Commit 03052018 @Kaustav

--- JobTrackerAdmin/Controllers/PaperController.cs
+++ JobTrackerAdmin/Controllers/PaperController.cs
@@ -21,5 +21,53 @@ namespace JobTrackerAdmin.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult PaperAddEdit(int id)
+        {
+            var data = pc.Papers.Find(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult PaperAddEdit(Paper collection)
+        {
+            if(collection.PaperID==0)
+            {
+                List<object> lst = new List<object>();
+
+                //lst.Add(collection.PaperID);
+                lst.Add(collection.PaperType);
+                lst.Add(collection.Rate);
+                lst.Add(collection.clientRemarks);
+                object[] allitems = lst.ToArray();
+                int output = pc.Database.ExecuteSqlCommand("insert into Papers(PaperType,Rate,clientRemarks) values (@p0,@p1,@p2)", allitems);
+                if(output>0)
+                {
+                    ViewBag.Itemmsg = "Paper Added Successfully";
+                }
+            }
+            else
+            {
+                List<object> lst = new List<object>();
+
+                lst.Add(collection.PaperID);
+                lst.Add(collection.PaperType);
+                lst.Add(collection.Rate);
+                lst.Add(collection.clientRemarks);
+                object[] allitems = lst.ToArray();
+                int output = pc.Database.ExecuteSqlCommand("Update Papers set PaperType=@p1,Rate=@p2,clientRemarks=@p3 where Paperid=@p0", allitems);
+                if (output > 0)
+                {
+                    ViewBag.Itemmsg = "Paper Updated Successfully";
+                }
+            }
+            
+            
+
+            return View();
+
+            
+        }
     }
 }
